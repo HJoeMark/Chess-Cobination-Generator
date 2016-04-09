6a9e02eab73f6925b1a3e165e99201fdcd7be88c
HJoeMark <hjoemark@gmail.com>
2016-04-09 14:32:52 +0200

    Evaluation improved

--- Chess-Combination-Generator/Chess-Combination-Generator/Evaluator.cs
+++ Chess-Combination-Generator/Chess-Combination-Generator/Evaluator.cs
@@ -18,44 +18,68 @@ namespace Chess_Combination_Generator
             return result;
         }
 
-        static int PiecePoint(FieldType[] board, bool isWhite = true)
+        //P = 100
+        //N = 320
+        //B = 330
+        //R = 500
+        //Q = 900
+
+        static int PiecePoint(FieldType[] board, bool isWhite = true, bool endGame = true)
         {
             var result = 0;
+            var index = -1;
             if (isWhite)
             {
                 foreach (var field in BoardInformations.InsideBoard)
                 {
+                    index++;
                     switch (board[field])
                     {
                         case FieldType.WhiteQueen:
+                            result += QueenPoint[index];
                             result += 900;
                             break;
                         case FieldType.WhiteRock:
+                            result += RockPoint[index];
                             result += 500;
                             break;
                         case FieldType.WhiteKnight:
-                            result += 300;
+                            result += KnightPoint[index];
+                            result += 320;
                             break;
                         case FieldType.WhiteBishop:
-                            result += 300;
+                            result += BishopPoint[index];
+                            result += 330;
                             break;
                         case FieldType.WhitePawn:
+                            result += PawnPoint[index];
                             result += 100;
                             break;
+                        case FieldType.WhiteKing:
+                            result += endGame ? KingPointEnd[index] : KingPointMiddle[index];
+                            break;
                         case FieldType.BlackQueen:
+                            result -= QueenPoint[63 - index];
                             result -= 900;
                             break;
                         case FieldType.BlackRock:
+                            result -= RockPoint[63 - index];
                             result -= 500;
                             break;
                         case FieldType.BlackKnight:
-                            result -= 300;
+                            result -= KnightPoint[63 - index];
+                            result -= 320;
                             break;
                         case FieldType.BlackBishop:
-                            result -= 300;
+                            result -= BishopPoint[63 - index];
+                            result -= 330;
                             break;
                         case FieldType.BlackPawn:
+                            result -= PawnPoint[63 - index];
                             result -= 100;
+                            break;
+                        case FieldType.BlackKing:
+                            result -= endGame ? KingPointEnd[63 - index] : KingPointMiddle[63 - index];
                             break;
                         default:
                             break;
@@ -66,37 +90,54 @@ namespace Chess_Combination_Generator
             {
                 foreach (var field in BoardInformations.InsideBoard)
                 {
+                    index++;
                     switch (board[field])
                     {
                         case FieldType.WhiteQueen:
+                            result -= QueenPoint[index];
                             result -= 900;
                             break;
                         case FieldType.WhiteRock:
+                            result -= RockPoint[index];
                             result -= 500;
                             break;
                         case FieldType.WhiteKnight:
-                            result -= 300;
+                            result -= KnightPoint[index];
+                            result -= 320;
                             break;
                         case FieldType.WhiteBishop:
-                            result -= 300;
+                            result -= BishopPoint[index];
+                            result -= 330;
                             break;
                         case FieldType.WhitePawn:
+                            result -= PawnPoint[index];
                             result -= 100;
                             break;
+                        case FieldType.WhiteKing:
+                            result -= endGame ? KingPointEnd[index] : KingPointMiddle[index];
+                            break;
                         case FieldType.BlackQueen:
+                            result += QueenPoint[63 - index];
                             result += 900;
                             break;
                         case FieldType.BlackRock:
+                            result += RockPoint[63 - index];
                             result += 500;
                             break;
                         case FieldType.BlackKnight:
-                            result += 300;
+                            result += KnightPoint[63 - index];
+                            result += 320;
                             break;
                         case FieldType.BlackBishop:
-                            result += 300;
+                            result += BishopPoint[63 - index];
+                            result += 330;
                             break;
                         case FieldType.BlackPawn:
+                            result += PawnPoint[63 - index];
                             result += 100;
+                            break;
+                        case FieldType.BlackKing:
+                            result += endGame ? KingPointEnd[63 - index] : KingPointMiddle[63 - index];
                             break;
                         default:
                             break;
@@ -105,5 +146,82 @@ namespace Chess_Combination_Generator
             }
             return result;
         }
+
+        #region PosPoints
+
+
+        static int[] RockPoint = new int[] {
+         0,0,0,0,0,0,0,0,
+         5,10,10,10,10,10,10,5,
+        -5,0,0,0,0,0,0,-5,
+        -5,0,0,0,0,0,0,-5,
+        -5,0,0,0,0,0,0,-5,
+        -5,0,0,0,0,0,0,-5,
+        -5,0,0,0,0,0,0,-5,
+         0,0,0,5,5,0,0,0
+        };
+
+        static int[] PawnPoint = new int[] {
+             0,  0,  0,  0,  0,  0,  0,  0,
+50, 50, 50, 50, 50, 50, 50, 50,
+10, 10, 20, 30, 30, 20, 10, 10,
+ 5,  5, 10, 25, 25, 10,  5,  5,
+ 0,  0,  0, 20, 20,  0,  0,  0,
+ 5, -5,-10,  0,  0,-10, -5,  5,
+ 5, 10, 10,-20,-20, 10, 10,  5,
+ 0,  0,  0,  0,  0,  0,  0,  0
+        };
+        static int[] KnightPoint = new int[] {
+            -50,-40,-30,-30,-30,-30,-40,-50,
+-40,-20,  0,  0,  0,  0,-20,-40,
+-30,  0, 10, 15, 15, 10,  0,-30,
+-30,  5, 15, 20, 20, 15,  5,-30,
+-30,  0, 15, 20, 20, 15,  0,-30,
+-30,  5, 10, 15, 15, 10,  5,-30,
+-40,-20,  0,  5,  5,  0,-20,-40,
+-50,-40,-30,-30,-30,-30,-40,-50
+        };
+        static int[] BishopPoint = new int[] {
+            -20,-10,-10,-10,-10,-10,-10,-20,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-10,  0,  5, 10, 10,  5,  0,-10,
+-10,  5,  5, 10, 10,  5,  5,-10,
+-10,  0, 10, 10, 10, 10,  0,-10,
+-10, 10, 10, 10, 10, 10, 10,-10,
+-10,  5,  0,  0,  0,  0,  5,-10,
+-20,-10,-10,-10,-10,-10,-10,-20
+        };
+        static int[] QueenPoint = new int[] {
+            -20,-10,-10, -5, -5,-10,-10,-20,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-10,  0,  5,  5,  5,  5,  0,-10,
+ -5,  0,  5,  5,  5,  5,  0, -5,
+  0,  0,  5,  5,  5,  5,  0, -5,
+-10,  5,  5,  5,  5,  5,  0,-10,
+-10,  0,  5,  0,  0,  0,  0,-10,
+-20,-10,-10, -5, -5,-10,-10,-20
+        };
+        static int[] KingPointMiddle = new int[] {
+            -30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-20,-30,-30,-40,-40,-30,-30,-20,
+-10,-20,-20,-20,-20,-20,-20,-10,
+ 20, 20,  0,  0,  0,  0, 20, 20,
+ 20, 30, 10,  0,  0, 10, 30, 20
+        };
+        static int[] KingPointEnd = new int[] {
+        -50,-40,-30,-20,-20,-30,-40,-50,
+-30,-20,-10,  0,  0,-10,-20,-30,
+-30,-10, 20, 30, 30, 20,-10,-30,
+-30,-10, 30, 40, 40, 30,-10,-30,
+-30,-10, 30, 40, 40, 30,-10,-30,
+-30,-10, 20, 30, 30, 20,-10,-30,
+-30,-30,  0,  0,  0,  0,-30,-30,
+-50,-30,-30,-30,-30,-30,-30,-50
+        };
+        #endregion
+
     }
 }
